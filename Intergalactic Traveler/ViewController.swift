e7b36a249a4aced33b64a8e1a4092f5fc890dcef
Diego Gonzalez <45858@psdschool.org>
2021-10-19 09:28:52 -0600

    Initial Commit

---
+++ Intergalactic Traveler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intergalactic Traveler
+//
+//  Created by Diego Gonzalez on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
