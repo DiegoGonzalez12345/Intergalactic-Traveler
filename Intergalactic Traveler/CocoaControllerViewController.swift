f2f26c76560c0201c9e7ac724887f4652486ce91
Diego Gonzalez <45858@psdschool.org>
2021-10-20 10:46:09 -0600

    Segue Complete

---
+++ Intergalactic Traveler/CocoaControllerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CocoaControllerViewController.swift
+//  Intergalactic Traveler
+//
+//  Created by Diego Gonzalez on 10/19/21.
+//
+
+import UIKit
+
+class CocoaControllerViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
